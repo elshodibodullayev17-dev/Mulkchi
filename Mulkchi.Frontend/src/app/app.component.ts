ae8763f6af4dc554ddc1a563539de6f44836c92a
Elshod Ibodullayev <elshodibadullayev28@gmail.com>
2026-03-10 23:20:01 +0500

    fix: remove double navbar, add port 4201 to CORS

--- Mulkchi.Frontend/src/app/app.component.ts
+++ Mulkchi.Frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
